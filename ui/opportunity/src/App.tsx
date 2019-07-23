113ffeb7e75744bee3c4937a2d94081b8cf7e7a9
Sam <malmas18@hotmail.com>
2019-07-23 21:58:03 +1000

    testing

--- ui/opportunity/src/App.tsx
+++ ui/opportunity/src/App.tsx
@@ -4,21 +4,21 @@ import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="au-grid">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h2 className="au-display-xl">Opportunities</h2>
+            <p>
+              Example paragraph. <a href="#">Internal link</a> and an <a href="#" rel="external">external link</a>.
+            </p>
+            <hr />
+            <p>
+              Lorem, Web Content Accessibility Guidelines (WCAG) ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
