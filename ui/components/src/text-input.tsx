fa08d81d0916dc9c06c002d408d4ebd768384373
Reshma Abraham <reshmaabraham@DTAs-MacBook-Pro.local>
2019-08-27 09:37:21 +1000

    adding files and packages to components

---
+++ ui/components/src/text-input.tsx
@@ -0,0 +1,18 @@
+
+/*
+  Light:  <AUtextInput>
+  Dark:   <AUtextInput dark>
+*/
+
+import React from 'react'; 
+import Formik from 'formik';
+import AUtextInput from '@gov.au/text-inputs';
+
+<Formik>
+    
+</Formik>
+
+/*
+<label htmlFor="text-input" >label text</label>
+<AUtextInput id="text-input" />
+*/ 
