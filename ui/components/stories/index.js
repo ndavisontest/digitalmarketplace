873a96b03dae486a3879608c3f7418fc2997dd8a
malmas18 <29763502+malmas18@users.noreply.github.com>
2019-09-19 09:05:18 +1000

    fix component builds

--- ui/components/stories/index.js
+++ ui/components/stories/index.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import TextInput from "../src/TextInput";
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -18,7 +19,7 @@ storiesOf('Button', module)
     </Button>
   ));
 
-storiesOf('TextInput', module)
+  storiesOf('TextInput', module)
   .add('with text', () => (
     <TextInput></TextInput>
   ));
